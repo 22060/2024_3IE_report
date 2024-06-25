a6a40fc273ac70bd66b0b9fed70e9686771ef77a
古城隆人 <22060@g.nagano-nct.ac.jp>
2024-06-25 10:54:33 +0900

    insert完成

--- algorithm/inseartsort/main.c
+++ algorithm/inseartsort/main.c
@@ -1,20 +1,49 @@
-
+#include <stdio.h>
 
 #define DATA 10
 
-void insert(int * a,int n,int n2);
+void insert(int * a,int n,int e);
 
-void insertsort(int a[], int n);
+void insertsort(int *a);
 
 int main(void){
-    int a[] = {1,3,5,7,9,2,4,6,8,10};
-    insert(a,5,0);
+    int a[DATA];
+    for (int i = 0; i < DATA; i++)
+    {
+        a[i] = DATA - i;
+    }
+    insertsort(a);
+    return 0;
 }
 
-void insert(int * a,int n,int n2){
-    for (int i = DATA; i > n; i--)
+void insertsort(int *a){
+    int t = 0;
+    for (int i = 1; i < DATA; i++)
+    {
+        t = i - 1;
+        while (a[t] > a[i]){
+            t--;
+            if(t == -1){
+                break;
+            }
+        }
+        insert(a,i,t+1);
+        for (int i = 0; i < DATA; i++)
+        {
+            printf("%d ",a[i]);
+        }
+        printf("\n");
+        
+        
+    }
+    
+}
+
+void insert(int * a,int n,int e){
+    int tmp = a[n];
+    for (int i = n; i > e; i--)
     {
         a[i] = a[i-1];
     }
-    a[n] = n2;
+    a[e] = tmp;
 }
