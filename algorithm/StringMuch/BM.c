353481f09443ec20cbc3e164386d77b522132f53
22060 <108120783+22060@users.noreply.github.com>
2024-07-16 14:31:55 +0900

    BM法完成

--- algorithm/StringMuch/BM.c
+++ algorithm/StringMuch/BM.c
@@ -2,12 +2,11 @@
 #define STR_NUM 2500
 int main(int argc, char *argv[])
 {
-    //変数の宣言
+    // 変数の宣言
     FILE *fp;
     char str[STR_NUM];
-    int t = 0;
-    //変数の初期化
-    char str2[100] = "doremifasorashido";
+    // 変数の初期化
+    char str2[100] = "energy";
     for (int i = 0; i < STR_NUM; i++)
     {
         str[i] = 0;
@@ -39,10 +38,10 @@ int main(int argc, char *argv[])
             // printf("%s", str);
         }
     }
-    //スキップテーブル作成
+    // スキップテーブル作成
     int skip[256];
     int size = 0;
-    while(str2[size] != 0)
+    while (str2[size] != 0)
     {
         size++;
     }
@@ -53,6 +52,25 @@ int main(int argc, char *argv[])
     for (int i = 0; i < size - 1; i++)
     {
         skip[str2[i]] = size - i - 1;
+    }
+    // ヤーッ！
+    int i = 0;
+    int t = 40;
+    while (t < STR_NUM)
+    {
+        if (i == size)
+        {
+            printf("find %d\n", t);
+        }
+        if (str[t + size - i - 1] == str2[size - i - 1])
+        {
+            i++;
+        }
+        else
+        {
+            i = 0;
+            t += skip[str[t + size - i - 1]];
+        }
     }
 
     return 0;
