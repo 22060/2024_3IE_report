5196f64e96366a0a64d1872c63dc8ce2200782dd
古城隆人 <22060@g.nagano-nct.ac.jp>
2024-10-23 11:58:38 +0900

    add file and change report.sh chmod

--- algorithm/multiple/main.c
+++ algorithm/multiple/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-    float x = -123456; // 0xfffe1dc0
+    int x = -123456; // 0xfffe1dc0
+    float y = -12.3456; // 0xc1458794
     unsigned char *p;
 
-    p = (unsigned char *)&x;
-    printf("%02x %02x %02x %02x\n", *p, *(p+1), *(p+2), *(p+3));
+    p = (unsigned char *)&y;
+    printf("%02x %02x %02x %02x\n", *(p), *(p+1), *(p+2), *(p+3));
+    for(int i = 0; i < 32; i++){
+        // printf("%d ",*(p+3 - (i/8)));
+        printf("%d", *(p+3 - (i/8))>>(7 - i%8) & 1);
+        printf("%c", i%8 == 7 ? ' ' : '\0');
+    }
 }
