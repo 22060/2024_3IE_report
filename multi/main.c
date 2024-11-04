a43909232fc3178b539b38290f0a8413012030bb
22060 <108120783+22060@users.noreply.github.com>
2024-11-04 11:02:36 +0900

    algo: dispNumber完成

--- multi/main.c
+++ multi/main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+
 #define KETA 30
 #define PLUS 1
 #define MINUS -1
-typedef struct{
+typedef struct
+{
     int n[KETA];
     int sgn;
-}Number;
+} Number;
 
 void clearByZero(Number *a);
 void dispNumber(Number *a);
-int main(){
+
+int main(int argc, char *argv[])
+{
     Number a;
     Number b;
     clearByZero(&a);
@@ -23,21 +27,27 @@ int main(){
     return 0;
 }
 
-void clearByZero(Number *a){
-    for(int i = 0; i < KETA; i++){
+void clearByZero(Number *a)
+{
+    for (int i = 0; i < KETA; i++)
+    {
         a->n[i] = 0;
     }
     a->sgn = 1;
 }
 
-void dispNumber(Number *a){
-    if(a->sgn == PLUS){
+void dispNumber(Number *a)
+{
+    if (a->sgn == PLUS)
+    {
         printf("+");
     }
-    else{
+    else
+    {
         printf("-");
     }
-    for(int i = KETA - 1; i >= 0; i--){
+    for (int i = KETA - 1; i >= 0; i--)
+    {
         printf(" %d", a->n[i]);
     }
 }
